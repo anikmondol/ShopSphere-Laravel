e337816e0ecefe23da0c12a72398bbb1ad541dad
anikmondol <anikmondol558363@gmail.com>
2024-11-26 12:00:42 +0600

    ShopSphere-Laravel Create Product Details Page complted.

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -13,4 +13,12 @@ class ShopController extends Controller
         $products = Product::orderBy("created_at", "DESC")->paginate(12);
         return view("shop", compact('products'));
     }
+
+
+    function product_details($product_slug) {
+        $product = Product::where('slug', $product_slug)->first();
+        $r_products = Product::where('slug','<>', $product_slug)->get()->take(8);
+        return view("details", compact('product', 'r_products'));
+    }
+
 }
