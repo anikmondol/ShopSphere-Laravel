903437fe18638ef21b1d7f2565f1638fef11b3cb
anikmondol <anikmondol558363@gmail.com>
2024-11-28 01:11:07 +0600

    ShopSphere-Laravel Move Product From Wishlist to Cart.

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -9,25 +9,36 @@ class WishlistController extends Controller
 {
 
 
-    function index(){
+    function index()
+    {
         $items = Cart::instance('wishlist')->content();
         return view('wishlist', compact('items'));
     }
 
-    function add_to_wishlist(Request $request){
+    function add_to_wishlist(Request $request)
+    {
         Cart::instance('wishlist')->add($request->id, $request->name, $request->quantity, $request->prince)->associate('App\Models\Product');
         return redirect()->back();
     }
 
-    function remove_item($rowId){
+    function remove_item($rowId)
+    {
         Cart::instance('wishlist')->remove($rowId);
         return redirect()->back();
     }
 
 
-    function empty_wishlist(){
+    function empty_wishlist()
+    {
         Cart::instance('wishlist')->destroy();
         return redirect()->back();
     }
 
+    function move_to_cart($rowId)
+    {
+        $item =  Cart::instance('wishlist')->get($rowId);
+        Cart::instance('wishlist')->remove($rowId);
+        Cart::instance('cart')->add($item->id,$item->name,$item->qty,$item->price)->associate('App\Models\Product');
+        return redirect()->back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.in
 Route::post('/wishlist/add', [WishlistController::class, 'add_to_wishlist'])->name('wishlist.add');
 Route::delete('/wishlist/item/remove/{rowId}', [WishlistController::class, 'remove_item'])->name('wishlist.item.remove');
 Route::delete('/wishlist/clear', [WishlistController::class, 'empty_wishlist'])->name('wishlist.empty');
+Route::post('/wishlist/move-to-cart/{rowId}', [WishlistController::class, 'move_to_cart'])->name('wishlist.move.to.cart');
 
 
 
