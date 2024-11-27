186fc571d3e15cc4339fadbd623c915a8372697c
anikmondol <anikmondol558363@gmail.com>
2024-11-27 13:19:45 +0600

    ShopSphere-Laravel  Products Filter By Price complted.

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -17,6 +18,9 @@ class ShopController extends Controller
         $size = $request->query('size') ? $request->query('size') : 12;
         $order = $request->query('order') ? $request->query('order') : -1;
         $f_brands = $request->query('brands');
+        $f_categories = $request->query('categories');
+        $min_price = $request->query('min') ? $request->query('min') : 1;
+        $max_price = $request->query('max') ? $request->query('max') : 500;
         switch ($order) {
             case 1:
                 $o_column = 'created_at';
@@ -41,12 +45,19 @@ class ShopController extends Controller
         }
 
         $brands = Brand::orderBy('name', 'ASC')->get();
-        $products = Product::where(function($query) use($f_brands){
-            $query->whereIn('brand_id',explode(',',$f_brands))->orwhereRaw("'".$f_brands."'=''");
+        $categories = Category::orderBy('name', 'ASC')->get();
+
+
+        $products = Product::where(function ($query) use ($f_brands) {
+            $query->whereIn('brand_id', explode(',', $f_brands))->orwhereRaw("'" . $f_brands . "'=''");
+        })->where(function ($query) use ($f_categories) {
+            $query->whereIn('category_id', explode(',', $f_categories))->orwhereRaw("'" . $f_categories . "'=''");
+        })->where(function ($query) use ($min_price, $max_price) {
+            $query->whereBetween('regular_price', [$min_price, $max_price])->orwhereBetween('sale_price', [$min_price, $max_price]);
         })->orderBy($o_column, $o_order)->paginate($size);
 
 
-        return view("shop", compact('products', 'size', 'order', 'brands', 'f_brands'));
+        return view("shop", compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories', 'max_price', 'min_price'));
     }
 
 
